b8bc97b7aed43c195c86adc926b9a9fd409c25ca
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-05-06 10:44:57 +0700

    Default Value error handling

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ const address = process.argv[2];
 if (!address) {
   return console.log("Please provide a location");
 } else {
-  geocode(address, (err, { latitude: lati, longtitude: long, location }) => {
-    if (err) {
-      return console.log("Error:", err);
-    }
-
-    forecast(lati, long, (error, data) => {
-      if (error) {
-        return console.log("Error:", error);
+  geocode(
+    address,
+    (err, { latitude: lati, longtitude: long, location } = {}) => {
+      if (err) {
+        return console.log("Error:", err);
       }
-      console.log(location);
-      console.log(data);
-    });
-  });
+
+      forecast(lati, long, (error, data) => {
+        if (error) {
+          return console.log("Error:", error);
+        }
+        console.log(location);
+        console.log(data);
+      });
+    }
+  );
 }
